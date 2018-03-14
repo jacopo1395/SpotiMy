b98bd6709177a3051ffbec48f676895d48777f55
jacopo <jack1395@hotmail.it>
2018-03-14 18:23:49 +0100

    save file

--- main.js
+++ main.js
@@ -2,23 +2,32 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 var path = require("path");
 var request = require('request');
-var SpotifyWebApi = require('spotify-web-api-node');
+// var SpotifyWebApi = require('spotify-web-api-node'); //not used
+var Spotify = require('node-spotify-api');
 var youtube = require('youtube-search');
+var lyric = require("lyric-get");
+var ms = require('mediaserver');
+const ffmpeg   = require('fluent-ffmpeg');
 var express = require('express');
 var app = express();
 
 
 
+
 var client_id = 'e5f7c755748b463cab7dcf25b83b017d'; // Your client id
 var client_secret = '8e569197dae149dc83e9f6add1fcc94e'; // Your secret
-var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+// var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
 // credentials are optional
-var spotifyApi = new SpotifyWebApi({
-    clientId: client_id,
-    clientSecret: client_secret,
-    redirectUri: redirect_uri
-});
+// var spotifyApi = new SpotifyWebApi({
+//     clientId: client_id,
+//     clientSecret: client_secret,
+//     redirectUri: redirect_uri
+// });
 
+var spotify = new Spotify({
+    id:client_id,
+    secret: client_secret
+});
 
 var send_error = function (res) {
     var response = {};
@@ -79,24 +88,46 @@ app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname+'/view/home.html'));
 });
 
-app.get('/find/:query', function(req, res) {
+app.get('/track/:query', function(req, res) {
     console.log(req.params);
     if (req.params.query===undefined) res.send("empty");
     else{
-        get_access_token(function (err) {
-        if (err) {
-            send_error(res);
-        }
-            spotifyApi.searchTracks(req.params.query, {limit:5})
-            .then(function(data) {
-                var tracks=data.body.tracks
-                res.json(tracks);
-            }, function(err) {
-                console.error(err);
-            });
+        spotify.search({ type: 'track', query: req.params.query, limit:5 }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+                res.json("{error}");
+            }
+            res.json(data);
         });
     }
 });
+app.get('/artist/:query', function(req, res) {
+    console.log(req.params);
+    if (req.params.query===undefined) res.send("empty");
+    else{
+        spotify.search({ type: 'artist', query: req.params.query, limit:5 }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+                res.json("{error}");
+            }
+            res.json(data);
+        });
+    }
+});
+app.get('/album/:query', function(req, res) {
+    console.log(req.params);
+    if (req.params.query===undefined) res.send("empty");
+    else{
+        spotify.search({ type: 'album', query: req.params.query, limit:5 }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+                res.json("{error}");
+            }
+            res.json(data);
+        });
+    }
+});
+
 
 app.get('/play/:query', function(req, res) {
     console.log(req.params.query);
@@ -111,30 +142,21 @@ app.get('/play/:query', function(req, res) {
             if(err) return console.log(err);
             var id = results[0].id;
             var url="http://www.youtube.com/watch?v="+id;
-            var stream = ytdl(url,{quality: 'highestaudio', filter: 'audioonly'});
+            var stream = ytdl(url,{quality: 'highestaudio'});
             console.log("stream music");
             // stream.pipe(fs.createWriteStream('audio.mp3'))
-            // stream.on('finish', function () {
-                // console.log("finish");
-                // var range = req.headers.range;
-                // const stats = fs.statSync("audio.mp3")
-                // var total = stats.size;
-                // var parts = range.replace(/bytes=/, "").split("-");
-                // var partialstart = parts[0];
-                // var partialend = parts[1];
-                //
-                // var start = parseInt(partialstart, 10);
-                // var end = partialend ? parseInt(partialend, 10) : total;
-                // var chunksize = (end-start);
-                // res.writeHead(206, {
-                //     "Content-Range": "bytes " + start + "-" + end + "/" + total,
-                //     "Accept-Ranges": "bytes",
-                //     "Content-Length": chunksize,
-                //     "Content-Type": "audio/ogg"
-                // });
-                // res.sendFile(__dirname+"/audio.mp3")
-            // });
-            stream.pipe(res);
+            stream.on('finish', function () {
+                console.log("finish");
+                // ms.pipe(req, res, __dirname+"/audio.mp3");
+            });
+            // stream.pipe(res);
+            ffmpeg(stream)
+            // .audioBitrate(128)
+            .save(__dirname+'/audio.flac')
+            .on('end', () => {
+                console.log('convertito!');
+                ms.pipe(req, res, __dirname+"/audio.flac");
+            });
 
         });
 
@@ -142,7 +164,59 @@ app.get('/play/:query', function(req, res) {
 
 });
 
-
+app.get('/lyric/:artist/:track', function(req, res){
+    lyric.get(req.params.artist, req.params.track, function(err, data){
+        if(err){
+            console.log(err);
+            var x={'lyric':''};
+            res.json(x);
+        }
+        else{
+            var r = {'lyric':data}
+            res.json(r);
+        }
+    });
+});
 
 app.listen(3000);
 console.log("Start server");
+
+
+
+// app.get('/artist/:query', function(req, res) {
+//     console.log(req.params);
+//     if (req.params.query===undefined) res.send("empty");
+//     else{
+//         get_access_token(function (err) {
+//         if (err) {
+//             send_error(res);
+//         }
+//             spotifyApi.searchArtists(req.params.query, {limit:5})
+//             .then(function(data) {
+//                 var tracks=data.body.tracks
+//                 res.json(tracks);
+//             }, function(err) {
+//                 console.error(err);
+//             });
+//         });
+//     }
+// });
+//
+// app.get('/album/:query', function(req, res) {
+//     console.log(req.params);
+//     if (req.params.query===undefined) res.send("empty");
+//     else{
+//         get_access_token(function (err) {
+//         if (err) {
+//             send_error(res);
+//         }
+//             spotifyApi.searchTracks(req.params.query, {limit:5, type: 'album'})
+//             .then(function(data) {
+//                 var tracks=data.body.tracks
+//                 res.json(tracks);
+//             }, function(err) {
+//                 console.error(err);
+//             });
+//         });
+//     }
+// });
